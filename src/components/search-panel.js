d56a9569fb4f6309de968187fffd6d278c10ee47
ilyagorev <gorev.ilya.it@gmail.com>
2018-11-05 20:02:59 +0700

    Стилизует search-panel

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
+
+import './search-panel.css';
 
 const SearchPanel = () => {
-
-    const searchText = 'Type here to search';
-
-    return <input placeholder={searchText}/>;
+    return (
+        <input type="text"
+               className="form-control search-input"
+               placeholder="type to search" />
+    );
 };
 
 export default SearchPanel;
